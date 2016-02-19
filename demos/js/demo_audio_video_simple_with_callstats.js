44591966edfc4b8f4f708e05a86abf25189da2b2
karthikbr82 <karthik@callstats.io>
2016-02-19 18:55:21 +0200

    callstats api sample file added in demos

---
+++ demos/js/demo_audio_video_simple_with_callstats.js
@@ -0,0 +1,92 @@
+var selfEasyrtcid = "";
+var selfRoomName = "";
+var callinitiator = false;
+
+var appId = "175240362";
+var appSecret = "Gvd126EUWQheaWQX9mwmeWIbzvs=";
+var callStats;
+
+function csInitCallback (err, msg){
+    console.log("CallStats Initializing Status: err= "+err+" msg= "+msg);
+}
+
+function connect() {
+    easyrtc.setVideoDims(640,480);
+    easyrtc.setRoomOccupantListener(convertListToButtons);
+
+    callStats = new callstats($,io,jsSHA);
+    easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+    easyrtc.setOnCall( function(easyrtcid, slot) {
+        console.log("call with " + easyrtcid + " established");
+        if (!callinitiator) {
+            var pc = easyrtc.getPeerConnectionByUserId(easyrtcid);
+            console.log("Pc is ",pc);
+            callStats.addNewFabric(pc, easyrtcid, callStats.fabricUsage.multiplex, selfRoomName);
+        }
+    });
+ }
+
+
+function clearConnectList() {
+    var otherClientDiv = document.getElementById('otherClients');
+    while (otherClientDiv.hasChildNodes()) {
+        otherClientDiv.removeChild(otherClientDiv.lastChild);
+    }
+}
+
+
+function convertListToButtons (roomName, data, isPrimary) {
+    selfRoomName = roomName;
+    clearConnectList();
+    var otherClientDiv = document.getElementById('otherClients');
+    for(var easyrtcid in data) {
+        var button = document.createElement('button');
+        button.onclick = function(easyrtcid) {
+            return function() {
+                performCall(easyrtcid);
+            };
+        }(easyrtcid);
+
+        var label = document.createTextNode(easyrtc.idToName(easyrtcid));
+        button.appendChild(label);
+        otherClientDiv.appendChild(button);
+    }
+}
+
+
+function performCall(otherEasyrtcid) {
+    easyrtc.hangupAll();
+    callinitiator = true;
+    var successCB = function(user, msg) {
+        console.log("success ",user,msg);
+        var pc = easyrtc.getPeerConnectionByUserId(otherEasyrtcid);
+        console.log("Pc is ",pc);
+        callStats.addNewFabric(pc, otherEasyrtcid, callStats.fabricUsage.multiplex, selfRoomName);
+    };
+    var failureCB = function() {
+
+    };
+
+    var acceptCB = function(wasAccepted, id) {
+        if( wasAccepted ){
+            console.log("call accepted by " + easyrtc.idToName(id));
+        }
+        else {
+            console.log("call rejected" + easyrtc.idToName(id));
+        }
+    }
+
+    easyrtc.call(otherEasyrtcid, successCB, failureCB, acceptCB);
+}
+
+
+function loginSuccess(easyrtcid) {
+    selfEasyrtcid = easyrtcid;
+    document.getElementById("iam").innerHTML = "I am " + easyrtc.cleanId(easyrtcid);
+    callStats.initialize(appId, appSecret, selfEasyrtcid, csInitCallback);
+}
+
+
+function loginFailure(errorCode, message) {
+    easyrtc.showError(errorCode, message);
+}
